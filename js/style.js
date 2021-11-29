fd2f0fbabbaad682d100ac05c1f558b86d92103c
albertobenito92 <abenitosuarez@fundacionpracticum.edu.es>
2021-11-29 11:45:38 +0100

    Cambios29/11/2021
    Cambios29/11/2021

---
+++ js/style.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+	 /*CURSOR*/
+	const cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px; display:block")
+    })
+    document.addEventListener('click', () => {
+        cursor.classList.add("cursorExpand");
+
+        setTimeout(() => {
+            cursor.classList.remove("cursorExpand");
+        }, 500)
+    });
+
+    /* FLECHA UP */
+    $(window).scroll(function(){
+    	if($(window).scrollTop() < 300){
+	    	$('.Flecha-Up').css('opacity', '0');
+	    }else{
+	    	$('.Flecha-Up').css('opacity', '1');
+	    };
+    }); 
+
+    $('.Flecha-Up').click(function(){
+    	$(window).scrollTop(0).slideDown("slow");
+    });
+
+    /* MENU OPEN RESPONSIVE*/
+    var anchoCompleto = screen.width;
+    var margenLeft = "-30%";
+
+    $('.MenuOpen').on('click', function() {
+    	$('#header').animate({
+    		marginLeft: 0,
+    		width: anchoCompleto,
+    	}, 500, function(){
+    		$('#header').css({"z-index": "99999"});
+    	});
+    });
+
+    $('.MenuClose').on('click', function() {
+    	$('#header').animate({
+    		marginLeft: margenLeft,
+    		width: 0,
+    	}, 500, function(){
+    		$('#header').css({"z-index": "1"});
+    	});
+    });
+
+    //MOSTRAR EN PANTALLA
+
+
+
+    /* EVENTOS SCROOL */
+	$(window).scroll( function(){
+
+		/*FadeIN elementos */
+	 	$('.Fade-In').each( function(i){
+			var top_of_element = $(this).offset().top;
+		    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+		    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+		    var top_of_screen = $(window).scrollTop();
+            
+            if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+                $(this).animate({'opacity':'1'},800); 
+            };
+        });
+
+	 	/*BARRA PROGRESO */
+        $('.barra').each( function(i){
+			var top_of_elementIdiomas = $(this).offset().top;
+		    var bottom_of_elementIdiomas = $(this).offset().top + $(this).outerHeight();
+		    var bottom_of_screenIdiomas = $(window).scrollTop() + $(window).innerHeight();
+		    var top_of_screenIdiomas = $(window).scrollTop();
+            
+            if ((bottom_of_screenIdiomas > top_of_elementIdiomas) && (top_of_screenIdiomas < bottom_of_elementIdiomas)){
+                $('.Html_Skills').animate({'width': 95+'%'},800);
+                $('.Photoshop_Skills').animate({'width': 90+'%'},800);
+                $('.Illustrator_Skills').animate({'width': 80+'%'},800);
+                $('.Javascript_Skills').animate({'width': 75+'%'},800);
+                $('.Jquery_Skills').animate({'width': 85+'%'},800);
+                $('.Angular_Skills').animate({'width': 70+'%'},800);
+                $('.Wordpress_Skills').animate({'width': 80+'%'},800);
+                $('.UXUI_Skills').animate({'width': 80+'%'},800);
+            };
+        });    	
+        
+
+	});
+
+	    
+});
